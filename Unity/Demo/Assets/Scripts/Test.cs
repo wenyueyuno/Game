02be16f636144a4281a4f741c7330db08dd4f5e2
wenyueyuno <wenyueyun@gmail.com>
2015-07-10 17:42:34 +0800

    删除

---
+++ Unity/Demo/Assets/Scripts/Test.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+/// <summary>
+/// @Intro: 
+/// @Binding to: Null 
+/// @Author: wenyueyun
+/// </summary>
+public class Test : ScrollRect{
+  
+	// Use this for initialization
+	void Start () {
+        
+	}
+    
+
+    public void init()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
